8ce8808462c2df23d230549f115898fa63201331
Mary Fesenko <m.fesenko17@gmail.com>
2020-02-15 19:42:43 +0800

    Add solution for day 9 of AoC 2019 (#7)

---
+++ 2019/puzzles/day9/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mfesenko/adventofcode/2019/intcode"
+)
+
+const (
+	_testMode        = int64(1)
+	_sensorBoostMode = int64(2)
+)
+
+func main() {
+	program, err := intcode.LoadProgram("input.txt")
+	if err != nil {
+		fmt.Printf("Failed to load input: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Part 1: %v\n", runBoostProgram(program, _testMode))
+	fmt.Printf("Part 2: %v\n", runBoostProgram(program, _sensorBoostMode))
+}
+
+func runBoostProgram(program *intcode.Program, mode int64) int64 {
+	computer := intcode.NewComputer()
+	computer.SetProgram(program)
+	computer.Input() <- mode
+	computer.Execute()
+
+	n := len(computer.Output())
+	output := make([]int64, n)
+	for i := 0; i < n; i++ {
+		output[i] = <-computer.Output()
+	}
+
+	if n == 1 {
+		return output[0]
+	}
+
+	fmt.Printf("Program execution failed: %v\n", output)
+	return -1
+}
