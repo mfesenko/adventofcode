d03fd7d1968892ae9af4e55dbca829af4b3de667
Mary Fesenko <m.fesenko17@gmail.com>
2020-03-05 21:26:27 +0800

    Remove unnecessary casting

--- 2019/asteroid/slope.go
+++ 2019/asteroid/slope.go
@@ -10,8 +10,8 @@ type slope struct {
 }
 
 func calculateSlope(a math.Point, b math.Point) slope {
-	dx := int64(a.X - b.X)
-	dy := int64(a.Y - b.Y)
+	dx := a.X - b.X
+	dy := a.Y - b.Y
 	gcd := math.GCD(math.Abs(dx), math.Abs(dy))
 	return slope{
 		dx / gcd,
